3fe614ed5d88137339370bfa0ca11c4a0aa3feec
Ankush <ankush.mitra@students.iiit.ac.in>
2019-11-05 13:58:18 +0530

    added basic piazzaHeader

---
+++ piazzaHeader.h
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include<bits/stdc++.h>
+#include <ctype.h>          
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include<math.h>
+#include<cstring>
+#include <unistd.h>
+#include <openssl/sha.h>
+#include <thread>
+#include <semaphore.h>
+#include <errno.h>
+#include <string.h>
+#define BUFF_SIZE 524288
+#define BUFF 10240
+#define BACK 2500
+using namespace std;
